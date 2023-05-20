30eff82f975296e841350268424a862088a03de6
Andrew Virts <asvirts@outlook.com>
2023-05-20 15:47:51 -0500

    problem #2

---
+++ algorithms-in-c#/Problem2.cs
@@ -0,0 +1,42 @@
+﻿/* 
+
+2. Write a C# Sharp program to get the absolute difference between n and 51. If n is broader than 51 return triple the absolute difference.
+
+Sample Input:
+53
+30
+51
+
+Expected Output:
+6
+21
+0
+
+ */
+
+using System;
+
+namespace exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(test(53));
+            Console.WriteLine(test(30));
+            Console.WriteLine(test(51));
+            Console.ReadLine();
+        }
+
+        public static int test(int n)
+        {
+            const int x = 51;
+
+            if (n > x)
+            {
+                return (n - x) * 3;
+            }
+            return x - n;
+        }
+    }
+}
