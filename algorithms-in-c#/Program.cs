8fba936bedb587a308b1e27dfb38daed3647a848
Andrew Virts <asvirts@outlook.com>
2023-05-20 13:40:38 -0500

    Add project files.

---
+++ algorithms-in-c#/Program.cs
@@ -0,0 +1,18 @@
+﻿/* 
+
+1. Write a C# Sharp program to compute the sum of the two numerical values. If the two values are the same, return triple their sum.
+
+Sample Input:
+1, 2
+3, 2
+2, 2
+
+Expected Output:
+3
+5
+12 
+
+ */
+
+
+Console.WriteLine("Hello, World!");
